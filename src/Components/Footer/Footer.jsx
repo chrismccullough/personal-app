14e662062c9328ec7a9ce2e4a0cbc7bd85098d74
Chris McCullough <cmccullough@gmail.com>
2018-01-24 15:01:37 -0800

    Initial Commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <header className="App-footer">
+        Footer
+      </header>
+    );
+  }
+}
+
+export default Footer;
