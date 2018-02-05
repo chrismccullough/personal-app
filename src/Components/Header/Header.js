00b0156edb68f5756e02f5196103e75429a0956c
Chris McCullough <cmccullough@gmail.com>
2018-02-05 15:02:01 -0800

    Updated header BG image Converted scroll down arrow to react

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { Link } from 'react-scroll'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { faChevronCircleDown } from '@fortawesome/fontawesome-free-solid'
 import NavData from '../../Stores/navigation'
 import Navigation from './Navigation'
 import Social from '../Social/Social'
@@ -18,8 +21,13 @@ class Header extends Component {
                 <div className="row banner">
                     <div className="banner-text">
                         <h1 className="responsive-headline">Chris McCullough</h1>
+
                         <h3>I am a <span>front end web developer</span> and <span>software engineer</span> based in Seattle, Washington.  I am passionate about developing standards-compliant web experiences using cutting edge languages and
-technologies; bringing my skills, experience, work ethic and thirst for learning to all new challenges and opportunities. Please <a className="smoothscroll" href="#about">start scrolling</a> to learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
+technologies; bringing my skills, experience, work ethic and thirst for learning to all new challenges and opportunities.</h3>
+                        <br/>
+                        <h3 className="center">
+                            Please <a className="smoothscroll" href="#about">start scrolling</a> to learn more <a className="smoothscroll" href="#about">about me</a>.
+                        </h3>
 
                         <hr />
 
@@ -29,9 +37,15 @@ technologies; bringing my skills, experience, work ethic and thirst for learning
                 </div>
 
                 <p className="scrolldown">
-                    <a className="smoothscroll" href="#about">
-                        <i className="icon-down-circle"></i>
-                    </a>
+                    <Link
+                        to={"about"}
+                        smooth={true}
+                        offset={0}
+                        duration={1000}
+                        delay={500}
+                    >
+                      <FontAwesomeIcon icon={faChevronCircleDown} />
+                    </Link>
                 </p>
             </header>
         );
